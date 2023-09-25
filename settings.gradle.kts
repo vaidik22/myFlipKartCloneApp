5f5f1f3f15677704399cfad5a6e7e13b915c18fe
ASUS <vaidik.nigam.jhs@gmail.com>
2023-09-25 15:57:38 +0530

    thank you page order confirmed

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Demo App"
+include(":app")
+dependencyResolutionManagement {
+    // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // Warning: this repository is going to shut down soon
+
+        // e.g this is how you would add jitpack
+        maven("https://jitpack.io")
+        // Add any repositories you would be adding to all projects here
+    }
+}
